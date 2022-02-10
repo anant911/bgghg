d5a24edede0cd65ef03e92e5cc8d447b3f4f0743
anant911 <77596400+anant911@users.noreply.github.com>
2022-02-09 20:50:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function preload() 
+{
+classifier=ml5.imageClassifier('DoodleNet');
+}
+
+function setup()
+{
+canvas= createCanvas(280, 280);
+canvas.center();
+background("white");
+canvas.mouseReleased(classifyCanvas);
+synth = window.speechSynthesis;
+}
+
+function draw()
+{
+strokeWeight(13);
+
+stroke(0);
+
+if(mouseIsPressed)
+{
+line(pmouseX, pmouseY, mouseX, mouseY);
+}
+}
+
+
+function clear()
+{
+    background("white");
+}
+ function classifyCanvas()
+ {
+     classifier.classify(canvas, gotResults);
+ }
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    console.log(results);
+    document.getElementById('label1').innerHTML = 'label:' + results[0].label;
+
+    document.getElementById('label2').innerHTML= 'Confidence:' + Math.round(results[0].confidence*100)+'%';
+
+    utterThis=new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
